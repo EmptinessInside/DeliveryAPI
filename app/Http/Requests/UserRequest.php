2b2b64cc1996ba6b8382a6952a69d4cf9bdba2c2
EmptinessInside <merkulovvladisalv2000@gmail.com>
2021-07-14 15:01:48 +0300

    #ED66 Добавлен кастомный запрос для пользователей с правилами

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'name' => 'required|string|max:50',
+            'last_name' => 'required|string|max:50',
+            'accept_mailing' => 'boolean',
+        ];
+
+        //TODO переключатель запросов с компановщиком правил (доработать)
+
+        switch ($this->getMethod()){
+            //Создание пользователя
+            CASE 'POST' :
+                return $rules + [
+                        'email' => 'required|string|max:200',
+                        'password' => 'required|string|min:6|max:200'
+                    ];
+            //Редактирование пользователя
+            CASE 'PUT' :
+                return $rules;
+        }
+    }
+
+    // TODO сообщения валидации
+
+}
