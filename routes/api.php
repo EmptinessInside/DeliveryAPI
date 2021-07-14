71b920658a78b679f468a6ab203a23b47427ba5a
EmptinessInside <merkulovvladisalv2000@gmail.com>
2021-07-14 14:58:49 +0300

    #ED66 Добавлен роутинг для работы с пользователем

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/create_user', [\App\Http\Controllers\UserController::class,'store']);
+Route::put('/edit_user/{id}', [\App\Http\Controllers\UserController::class, 'update']);
+Route::get('/get_user/{id}', [\App\Http\Controllers\UserController::class, 'show']);
