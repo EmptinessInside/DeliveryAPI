6383582a1bd42752c9d814ba4b624edbb2dafb91
EmptinessInside <merkulovvladisalv2000@gmail.com>
2021-07-14 15:16:36 +0300

    #ED66 Добавлена миграция для адресов доставок

---
+++ database/migrations/2021_07_14_121221_create_delivery_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliveryAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('delivery_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('region', 200);
+            $table->string('locality', 60);
+            $table->string('street', 60);
+            $table->integer('house');
+            $table->integer('index');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('delivery_addresses');
+    }
+}
