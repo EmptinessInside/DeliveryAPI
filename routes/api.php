82622cb76586e19b91d3425c3bb5048a12755d0b
EmptinessInside <merkulovvladisalv2000@gmail.com>
2021-07-14 16:03:50 +0300

    #ED66 Добавлен api роуты для адресов доставок

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/create_user', [\App\Http\Controllers\UserController::class,'store']);
 Route::put('/edit_user/{id}', [\App\Http\Controllers\UserController::class, 'update']);
 Route::get('/get_user/{id}', [\App\Http\Controllers\UserController::class, 'show']);
+
+Route::resource('/delivery_address', '\App\Http\Controllers\DeliveryAddressController')->only(
+  'store', 'update', 'destroy'
+);
